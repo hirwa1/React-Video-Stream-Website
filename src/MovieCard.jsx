c7a1a2a16982611d8056b1fe9016216ff9eee9f4
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 17:15:10 +0200

    iop

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 const MovieCard = () => {
     return (
+              <div className='movie'>
 
+                   <div>
+                   <p>{movie1.year} </p>
+                   </div>
+
+                  <div>
+                  <img  src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://kigalidevelopers.com/wp-content/uploads/2022/03/TRUSTED-KIGALI-DEVELOPERS.png'}  alt={movie1.Title}/>
+                  </div>
+                  
+                  <div>
+                      <span>{movie1.Type}</span>
+                      <h3>{movie1.Title}</h3>
+                  </div>
+               </div>
     );
 }
 
