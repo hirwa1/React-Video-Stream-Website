59724b3649de1c5158c83942a47fc5df098528ae
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:37:03 +0200

    iop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+const API_URL = 'http://www.omdbapi.com/?apikey=b3063901'
 
 const App = () =>{
-    return(
-<h1>App</h1>
+    return (
+         <h1>App</h1>
     );
 }
 
