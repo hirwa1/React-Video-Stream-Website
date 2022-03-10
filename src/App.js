6287c05b15cd402c53c5a1e0e98f38fdc8a8f9fe
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:57:27 +0200

    gh

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import SearchIcon from './search.svg';
 const API_URL = 'http://www.omdbapi.com/?apikey=b3063901';
 
 
-
+const movie1 = {
+   " Poster": "https://m.media-amazon.com/images/M/MV5BYWM0MDI1ZmItZTYzNi00ZWVlLTg5MTctNzllNjY2ZTI3NDhhXkEyXkFqcGdeQXVyNDk5MjA2MQ@@._V1_SX300.jpg",
+"Title": "Reign of Judges: Title of Liberty - Concept Short",
+"Type": "movie",
+"Year": "2018",
+"imdbID": "tt4275958"
+}
 const App = () =>{
     const searchMovies = async (title) =>{
         const response = await fetch(`${API_URL}&s={title}`);
