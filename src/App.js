6fe76ac63727792dacc200b43d05bd28888cd459
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:14:43 +0200

    fg

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-       <button> -</button>
+       <button onClick={() => setCounter((prveCount) => prveCount - 1)}> -</button>
        <h1> {counter}</h1>
        <button>+</button>
     </div>
