63e1c4e41ababd54a93d4333ec0d0684317b8f32
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:08:43 +0200

    iop

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-const  Person = (props) => {
-  return (
-    <>
-     <h1> Name : {props.name}</h1>
-     <h2> Last Name: {props.last}</h2>
-     <h2> Age : {props.age}</h2>
-    </>
-  )
-}
+
 function App() {
 
   return (
     <div className="App">
-      <Person name={'Hirwa'} last={'NIYIBZI'} age={'21'}/>
-      <Person name={'John'} last={'Money'} age={'34'}/>
-      <Person name={'Lord'} last={'Hirwao'} age={'35'}/>
+      
     </div>
   );
 }
