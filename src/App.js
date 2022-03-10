312f7c6dd4191d818cd4a9c6814ed8b86f32cecc
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:10:45 +0200

    op

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import  { useState} from 'react';
 import './App.css';
 
 function App() {
- const [counter, setCounter] = useState();
+ const [counter, setCounter] = useState(0);
   return (
     <div className="App">
        <button> -</button>
