e74b9ae1c2780ff1feb709c796174bb7e263d318
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 17:14:43 +0200

    iop

--- src/App.js
+++ src/App.js
@@ -41,18 +41,7 @@ const App = () =>{
           </div>
 
           <div className='container'>
-               <div className='movie'>
-                   <div>
-                   <p>{movie1.year} </p>
-                   </div>
-                  <div>
-                  <img  src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://kigalidevelopers.com/wp-content/uploads/2022/03/TRUSTED-KIGALI-DEVELOPERS.png'}  alt={movie1.Title}/>
-                  </div>
-                  <div>
-                      <span>{movie1.Type}</span>
-                      <h3>{movie1.Title}</h3>
-                  </div>
-               </div>
+               
           </div>
         </div>
     );
