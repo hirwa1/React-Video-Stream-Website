08cd7972242243e52435a2bdc96d79234459ed54
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:03:55 +0200

    iop

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 const  Person = (props) => {
   return (
     <>
-     <h1> Name : </h1>
-     <h2> Last Name: Doe</h2>
+     <h1> Name : {props.name}</h1>
+     <h2> Last Name: {props.last}</h2>
      <h2> Age : 30</h2>
     </>
   )
