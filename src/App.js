06330138e90ca38d7c8838aa3c48dad31867807f
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:38:53 +0200

    iop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useEffect } from 'react';
 const API_URL = 'http://www.omdbapi.com/?apikey=b3063901';
 
+
+
 const App = () =>{
+    useEffect(() => {
+
+    }, []);
     return (
          <h1>App</h1>
     );
