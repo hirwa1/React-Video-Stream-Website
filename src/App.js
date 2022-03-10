ef7e3b30283ee2e9180e192e6a5f33c6c994150b
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:45:20 +0200

    iop

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () =>{
     const searchMovies = async (title) =>{
         const response = await fetch(`${API_URL}&s={title}`);
         const data = await response.json();
-        console.log(data);
+        console.log(data.Search);
     }
     useEffect(() => {
        searchMovies('Spiderman');
