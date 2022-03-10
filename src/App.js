b1c42ef49ce5da7c7370b07b97e75659995b7f53
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 17:13:14 +0200

    iop

--- src/App.js
+++ src/App.js
@@ -46,8 +46,6 @@ const App = () =>{
                    <p>{movie1.year} </p>
                    </div>
                   <div>
-
-                  
                   <img  src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://kigalidevelopers.com/wp-content/uploads/2022/03/TRUSTED-KIGALI-DEVELOPERS.png'}  alt={movie1.Title}/>
                   </div>
                   <div>
