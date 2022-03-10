5bde08f83fd2f7016e33d9d65e1a5b96ca47fd7c
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-03-10 16:43:02 +0200

    uiop

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ const API_URL = 'http://www.omdbapi.com/?apikey=b3063901';
 
 const App = () =>{
     const searchMovies = async (title) =>{
-        const responce = await fetch(`${API_URL}&s={title}`)
+        const response = await fetch(`${API_URL}&s={title}`);
+        const data = await response.json();
+        console.log(data);
     }
     useEffect(() => {
-
+       searchMovies('Spiderman');
     }, []);
     return (
          <h1>App</h1>
